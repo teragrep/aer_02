589312fb6096e96f044c016e2133ff804b4edf72
eemhu <125959687+eemhu@users.noreply.github.com>
2025-02-20 14:10:37 +0200

    remove multi-record processing from DefaultPlugin; add multi-record processing to EventDataConsumer; remove JsonRecords with tests, add EventRecords with tests; add testSyslogBridgeWithJsonRecordsDataAndUnknownTypes test

--- src/main/java/com/teragrep/aer_02/plugin/DefaultPlugin.java
+++ src/main/java/com/teragrep/aer_02/plugin/DefaultPlugin.java
@@ -45,7 +45,6 @@
  */
 package com.teragrep.aer_02.plugin;
 
-import com.teragrep.aer_02.json.JsonRecords;
 import com.teragrep.akv_01.event.ParsedEvent;
 import com.teragrep.akv_01.plugin.Plugin;
 import com.teragrep.nlf_01.PropertiesJson;
@@ -54,7 +53,6 @@ import com.teragrep.rlo_14.Facility;
 import com.teragrep.rlo_14.SDElement;
 import com.teragrep.rlo_14.Severity;
 import com.teragrep.rlo_14.SyslogMessage;
-import jakarta.json.JsonException;
 
 import java.time.Instant;
 import java.time.ZonedDateTime;
@@ -78,7 +76,6 @@ public final class DefaultPlugin implements Plugin {
 
     @Override
     public List<SyslogMessage> syslogMessage(final ParsedEvent parsedEvent) {
-        final List<SyslogMessage> syslogMessages = new ArrayList<>();
         ZonedDateTime enqueuedTime;
         if (!parsedEvent.enqueuedTimeUtc().isStub()) {
             enqueuedTime = parsedEvent.enqueuedTimeUtc().zonedDateTime();
@@ -133,31 +130,29 @@ public final class DefaultPlugin implements Plugin {
         final SDElement sdComponentInfo = new SDElement("aer_02@48577")
                 .addSDParam("timestamp_source", parsedEvent.enqueuedTimeUtc().isStub() ? "generated" : "timeEnqueued");
 
-        List<String> records = new ArrayList<>();
-        if (parsedEvent.isJsonStructure()) {
-            JsonRecords jsonRecords = new JsonRecords(parsedEvent.asJsonStructure());
-            try {
-                records = jsonRecords.records();
-            }
-            catch (JsonException ignored) {
-                records.add(parsedEvent.asString());
-            }
-        }
-        else {
-            records.add(parsedEvent.asString());
-        }
-
         String seqNum = "";
         if (!parsedEvent.systemProperties().isStub()) {
             seqNum = String.valueOf(parsedEvent.systemProperties().asMap().getOrDefault("SequenceNumber", "0"));
         }
 
-        for (final String record : records) {
-            syslogMessages
-                    .add(new SyslogMessage().withSeverity(Severity.INFORMATIONAL).withFacility(Facility.LOCAL0).withTimestamp(!parsedEvent.enqueuedTimeUtc().isStub() ? enqueuedTime.toInstant().toEpochMilli() : Instant.now().toEpochMilli()).withHostname(syslogHostname).withAppName(syslogAppname).withSDElement(sdId).withSDElement(sdPartition).withSDElement(sdEvent).withSDElement(sdComponentInfo).withMsgId(seqNum).withMsg(record));
-        }
+        final SyslogMessage syslogMessage = new SyslogMessage()
+                .withSeverity(Severity.INFORMATIONAL)
+                .withFacility(Facility.LOCAL0)
+                .withTimestamp(
+                        !parsedEvent.enqueuedTimeUtc().isStub() ? enqueuedTime.toInstant().toEpochMilli() : Instant
+                                .now()
+                                .toEpochMilli()
+                )
+                .withHostname(syslogHostname)
+                .withAppName(syslogAppname)
+                .withSDElement(sdId)
+                .withSDElement(sdPartition)
+                .withSDElement(sdEvent)
+                .withSDElement(sdComponentInfo)
+                .withMsgId(seqNum)
+                .withMsg(parsedEvent.asString());
 
-        return syslogMessages;
+        return Collections.singletonList(syslogMessage);
     }
 
     @Override
