c428f9d06741b6628c1483d2486de316e08d3c90
Motoko Kusanagi <kordex@gmail.com>
2025-10-02 15:49:10 +0300

    add Level.FINE counter for events that match sekvenssi_daemonset_

--- src/main/java/com/teragrep/aer_02/SyslogBridge.java
+++ src/main/java/com/teragrep/aer_02/SyslogBridge.java
@@ -112,6 +112,18 @@ public class SyslogBridge {
     ) {
         try {
             if (context.getLogger().isLoggable(Level.FINE)) {
+                long sekvenssiCount = 0;
+                for (String event : events) {
+                    if (event.matches(".*sekvenssi_daemonset_.*")) {
+                        sekvenssiCount++;
+                    }
+                }
+
+                context.getLogger().fine("sekvenssi_daemonset_ count: " + sekvenssiCount);
+            }
+
+
+            if (context.getLogger().isLoggable(Level.FINE)) {
                 context.getLogger().fine("eventHubTriggerToSyslog triggered");
                 context.getLogger().fine("Got events: <[" + events.length + "]>");
             }
